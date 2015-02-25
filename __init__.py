7268ccad88ecbf85b97fe0fbb4ce5436b4043f76
DmitriyObritskiy <DmitriyObritskiy@gmail.com>
2015-02-25 22:27:25 +0300

    *

--- __init__.py
+++ __init__.py
@@ -1,21 +1,44 @@
-# -*- coding: utf-8 -*-
+# coding: cp1251
 class Account(object):
-
-    def __init__(self, accnum):
+    def __init__(self, accnum, bic):
+        self.dict_cl_acc = {'A': '0', 'À': '0',
+                            'B': '1', 'Â': '1',
+                            'C': '2', 'Ñ': '2',
+                            'E': '3', 'Å': '3',
+                            'H': '4', 'Í': '4',
+                            'K': '5', 'Ê': '5',
+                            'M': '6', 'Ì': '6',
+                            'P': '7', 'Ð': '7',
+                            'T': '8', 'Ò': '8',
+                            'X': '9', 'Õ': '9'}
         self.accnum = str(accnum)
+        self.bic = bic
+        self.rkcnum = "0" + self.bic[4:6]
+        self.key = self.accnum[8:9]
+        self.currency = self.accnum[5:6]
+        # self.normaccnum = self.rkcnum + self.accnum[0:8] + "0" + self.accnum[9:20]
 
-    def checkacckey(self, bic):
+        self.normaccnum = self.accnum[0:8] + "0" + self.accnum[9:20]
+
+        if self.currency in self.dict_cl_acc:
+            self.normaccnum = self.normaccnum.replace(self.currency, self.dict_cl_acc[self.currency])
+        self.normaccnum = self.rkcnum + self.normaccnum
+
+    def calcacckey(self):
         mask = "71371371371371371371371"
         summ = 0
-        print len(self.accnum)
-        for i in range(len(self.accnum)):
-            print self.accnum[i], mask[i], str(int(self.accnum[i]) * int(mask[i]))
-            summ += int(self.accnum[i]) * int(mask[i])
-            print "Summ = %s", % (summ)
+        for i in range(len(self.normaccnum)):
+            # print self.accnum[i], mask[i], str(int(self.accnum[i]) * int(mask[i]))
+            summ += int(self.normaccnum[i]) * int(mask[i])
+        print("Summ : {0}".format(str(summ)))
 
 
-
-acc1 = Account("40702810100000000010")
-print acc1.accnum
-
-acc1.checkacckey("87687")
+acc1 = Account("40702Å99700000000010", "044579203")
+print("accnum ", acc1.accnum)
+print("normaccnum", acc1.normaccnum)
+print("bic", acc1.bic)
+print("key", acc1.key)
+print("currency", acc1.currency)
+print("rkcnum", acc1.rkcnum)
+print("dict B lat", acc1.dict_cl_acc["Â"])
+acc1.calcacckey()
